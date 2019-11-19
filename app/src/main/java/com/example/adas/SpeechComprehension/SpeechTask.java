40a4ef6d9cfe0413331a84526a99c8aa6036dba7
gureisu <48702304+gureisu@users.noreply.github.com>
2019-11-19 16:25:50 +1300

    made some changes

--- app/src/main/java/com/example/adas/SpeechComprehension/SpeechTask.java
+++ app/src/main/java/com/example/adas/SpeechComprehension/SpeechTask.java
@@ -10,13 +10,18 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
 
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-;
+
+import com.example.adas.Model.Item;
+import com.example.adas.Model.SpeechRecogntionQuestion;
 
 import com.example.adas.R;
 
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class SpeechTask extends AppCompatActivity {
@@ -29,28 +34,105 @@ public class SpeechTask extends AppCompatActivity {
     private RadioButton mButtonChoice2;
     private RadioButton mButtonChoice3;
     private RadioButton mButtonChoice4;
+    private RadioGroup radiogroup;
     private Button mSubmitButton;
     TextToSpeech tts;
     private String mAnswer;
     private int mScore = 0;
     private int mQuestionNumber = 0;
-    int counter = 4;
+    int counter = 6;
+    int itemcounter = 0;
 
 
+    private SpeechRecogntionQuestion mquestion;
 
+    public void initializeQuestion(){
+        mquestion = new SpeechRecogntionQuestion();
+        mquestion.setPhrase("In the freezing ocean waters of Antarctica,\n" +
+                "the planet's largest seals make their home in a\n" +
+                "frozen world. These giants are southern elephant\n" +
+                "seals, and they can grow as long as the length of a\n" +
+                "car and weigh as much as two cars combined. The\n" +
+                "name “elephant seal” comes from both the males'\n" +
+                "enormous size and from their giant trunk-like nose,\n" +
+                "called a proboscis");
+
+        Item item1 = new Item();
+        Item item2 = new Item();
+        Item item3 = new Item();
+        Item item4 = new Item();
+        Item item5 = new Item();
+        Item item6 = new Item();
+
+        item1.setQuestion("question1?");
+        item1.setChoice1("yes");
+        item1.setChoice2("no");
+        item1.setChoice3("maybe");
+        item1.setChoice4("idk");
+        item1.setAnswer("yes");
+
+        item2.setQuestion("question2?");
+        item2.setChoice1("yes");
+        item2.setChoice2("no");
+        item2.setChoice3("maybe");
+        item2.setChoice4("idk");
+        item2.setAnswer("yes");
+
+        item3.setQuestion("question3?");
+        item3.setChoice1("yes");
+        item3.setChoice2("no");
+        item3.setChoice3("maybe");
+        item3.setChoice4("idk");
+        item3.setAnswer("yes");
+
+        item4.setQuestion("question4?");
+        item4.setChoice1("yes");
+        item4.setChoice2("no");
+        item4.setChoice3("maybe");
+        item4.setChoice4("idk");
+        item4.setAnswer("yes");
+
+        item5.setQuestion("question5?");
+        item5.setChoice1("yes");
+        item5.setChoice2("no");
+        item5.setChoice3("maybe");
+        item5.setChoice4("idk");
+        item5.setAnswer("yes");
+
+        item6.setQuestion("question6?");
+        item6.setChoice1("yes");
+        item6.setChoice2("no");
+        item6.setChoice3("maybe");
+        item6.setChoice4("idk");
+        item6.setAnswer("yes");
+
+        ArrayList<Item> items = new ArrayList<Item>();
+
+        items.add(item1);
+        items.add(item2);
+        items.add(item3);
+        items.add(item4);
+        items.add(item5);
+        items.add(item6);
+
+        mquestion.setListofitems(items);
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.speech_task);
 
+        initializeQuestion();
+        mScoreView = findViewById(R.id.score);
+        mQuestionView =  findViewById((R.id.question));
+        mButtonChoice1 =  findViewById(R.id.choice1);
+        mButtonChoice2 =  findViewById(R.id.choice2);
+        mButtonChoice3 = findViewById(R.id.choice3);
+        mButtonChoice4 = findViewById(R.id.choice4);
+        mSubmitButton = findViewById(R.id.submitButton);
 
-        mScoreView = (TextView) findViewById(R.id.score);
-        mQuestionView = (ImageButton) findViewById((R.id.question));
-        mButtonChoice1 = (RadioButton) findViewById(R.id.choice1);
-        mButtonChoice2 = (RadioButton) findViewById(R.id.choice2);
-        mButtonChoice3 = (RadioButton) findViewById(R.id.choice3);
-        mButtonChoice4 = (RadioButton) findViewById(R.id.choice4);
-        mSubmitButton = (Button) findViewById(R.id.submitButton);
+        radiogroup = findViewById(R.id.radiogroup);
         updateQuestion();
 
 
@@ -60,72 +142,18 @@ public class SpeechTask extends AppCompatActivity {
             {
                 if (counter != 0)
                 {
-                    switch(mQuestionNumber)
-                    {
-                        case 1:
-                        {
-                            if (mButtonChoice1.isChecked())
-                            {
-                                mScore = mScore + 1;
-                                updateScore(mScore);
-                                updateQuestion();
-                            }
-                            else
-                            {
-                                Toast.makeText(SpeechTask.this, "WRONG", Toast.LENGTH_SHORT).show();
-                                updateQuestion();
-                            }
-                        }
-                        break;
-                        case 2:
-                        {
-                            if (mButtonChoice2.isChecked())
-                            {
-                                mScore = mScore + 1;
-                                updateScore(mScore);
-                                updateQuestion();
-                            }
-                            else
-                            {
-                                Toast.makeText(SpeechTask.this, "WRONG", Toast.LENGTH_SHORT).show();
-                                updateQuestion();
-                            }
-                        }
-                        break;
-                        case 3:
-                        {
-                            if (mButtonChoice3.isChecked())
-                            {
-                                mScore = mScore + 1;
-                                updateScore(mScore);
-                                updateQuestion();
-                            }
-                            else
-                            {
-                                Toast.makeText(SpeechTask.this, "WRONG", Toast.LENGTH_SHORT).show();
-                                updateQuestion();
-                            }
-                        }
-                        break;
-                        case 4:
-                        {
-                            if (mButtonChoice4.isChecked())
-                            {
-                                mScore = mScore + 1;
-                                updateScore(mScore);
-                                updateQuestion();
-                            }
-                            else
-                            {
-                                Toast.makeText(SpeechTask.this, "WRONG", Toast.LENGTH_SHORT).show();
-                                updateQuestion();
-                            }
-                        }
-                        break;
+                    counter--;
+
+                    if (checkAnswer())
+                {
+                    mScore = mScore + 1;
+                    updateScore(mScore);
+                    updateQuestion();
                 }
-                counter--;
-
-
+                else{
+                    Toast.makeText(SpeechTask.this, "WRONG", Toast.LENGTH_SHORT).show();
+                               updateQuestion();
+                }
 
 
                }
@@ -156,29 +184,46 @@ public class SpeechTask extends AppCompatActivity {
             public void onClick(View view) {
                 if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
                 {
-                    tts.speak(mQuestionLibrary.getQuestion(mQuestionNumber-1), TextToSpeech.QUEUE_FLUSH,null,null);
+                    tts.speak(mquestion.getListofitems().get(itemcounter).getQuestion(), TextToSpeech.QUEUE_FLUSH,null,null);
                 }
                 else
                 {
-                    tts.speak(mQuestionLibrary.getQuestion(mQuestionNumber-1), TextToSpeech.QUEUE_FLUSH,null);
+                    tts.speak(mquestion.getListofitems().get(itemcounter).getQuestion(), TextToSpeech.QUEUE_FLUSH,null);
                 }
             }
         });
 
-        mButtonChoice1.setText(mQuestionLibrary.getChoice1(mQuestionNumber));
-        mButtonChoice2.setText(mQuestionLibrary.getChoice2(mQuestionNumber));
-        mButtonChoice3.setText(mQuestionLibrary.getChoice3(mQuestionNumber));
-        mButtonChoice4.setText(mQuestionLibrary.getChoice4(mQuestionNumber));
+        Item temp = mquestion.getListofitems().get(itemcounter);
 
-        mAnswer = mQuestionLibrary.getCorrectAnswer(mQuestionNumber);
-        mQuestionNumber++;
+        mButtonChoice1.setText(temp.getChoice1());
+        mButtonChoice2.setText(temp.getChoice2());
+        mButtonChoice3.setText(temp.getChoice3());
+        mButtonChoice4.setText(temp.getChoice4());
+
+
+
         mButtonChoice1.setChecked(false);
         mButtonChoice2.setChecked(false);
         mButtonChoice3.setChecked(false);
         mButtonChoice4.setChecked(false);
+
+        itemcounter++;
     }
 
     private void updateScore(int point) {
         mScoreView.setText("" + mScore);
+    }
+
+    private boolean checkAnswer(){
+        int selectedId = radiogroup.getCheckedRadioButtonId();
+        RadioButton rb = radiogroup.findViewById(selectedId);
+        String tempAnswer = rb.getText().toString();
+        String rightanswer = mquestion.getListofitems().get(itemcounter).getAnswer();
+        if (tempAnswer.equals(rightanswer)){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }

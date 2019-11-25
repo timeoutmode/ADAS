98aa06ebaea4e0cd12b9d8c3225da983b57f714f
Brian <cama31@manukaumail.com>
2019-11-25 18:56:30 +1300

    added parcelable Result object and made Patient object into parcelable

--- app/src/main/java/com/example/adas/Model/Patient.java
+++ app/src/main/java/com/example/adas/Model/Patient.java
@@ -1,14 +1,27 @@
 package com.example.adas.Model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Date;
 
-public class Patient {
+public class Patient implements Parcelable {
     private String patientID, firstName, lastName, address, ethnicity, gender;
     private Date birthDate;
 
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        @Override
+        public Object createFromParcel(Parcel parcel) {
+            return new Patient(parcel);
+        }
+
+        @Override
+        public Object[] newArray(int i) {
+            return new Patient[i];
+        }
+    };
+
     public Patient() {}
-
-
 
     public void setPatientID(String patientID) {
         this.patientID = patientID;
@@ -24,6 +37,35 @@ public class Patient {
         this.birthDate = birthDate;
     }
 
+    // Constructor for creating result object from parcel
+    public Patient(Parcel parcel) {
+        this.patientID = parcel.readString();
+        this.firstName = parcel.readString();
+        this.lastName = parcel.readString();
+        this.address = parcel.readString();
+        this.ethnicity = parcel.readString();
+        this.gender = parcel.readString();
+        if(parcel.readLong() != -1L) {
+            this.birthDate =  new Date(parcel.readLong());
+        }
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(this.patientID);
+        parcel.writeString(this.firstName);
+        parcel.writeString(this.lastName);
+        parcel.writeString(this.address);
+        parcel.writeString(this.ethnicity);
+        parcel.writeString(this.gender);
+        parcel.writeLong(this.birthDate != null ? birthDate.getTime() : -1L);
+    }
 
     public String getPatientID() {
         return patientID;

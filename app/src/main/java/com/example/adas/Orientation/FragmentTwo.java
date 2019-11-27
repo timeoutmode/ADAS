aa7c74534d570364ce614cb5bfb675db39696679
gureisu <48702304+gureisu@users.noreply.github.com>
2019-11-27 20:55:20 +1300

    added fragment 2 seasons codes

--- app/src/main/java/com/example/adas/Orientation/FragmentTwo.java
+++ app/src/main/java/com/example/adas/Orientation/FragmentTwo.java
@@ -8,10 +8,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 import com.example.adas.R;
 
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class FragmentTwo extends Fragment {
+
+private RadioGroup radioGroup;
+private RadioButton choice1, choice2, choice3, choice4;
 
 
     public FragmentTwo() {
@@ -21,8 +32,30 @@ public class FragmentTwo extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) { //values of radio buttons
+                String monthSeasons[] = {
+                        "Winter", "Winter",
+                        "Spring", "Spring", "Spring",
+                        "Summer", "Summer", "Summer",
+                        "Fall", "Fall", "Fall",
+                        "Winter"
+                };
+
+                java.util.Date date = new Date();
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(date);
+                int month = cal.get(Calendar.MONTH);// gets current month
+
+                System.out.println("This is the current season:"+ monthSeasons[month]); // checks the current season 
+            }
+        });
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.activity_fragment_two, container, false);
+
+
     }
 
 }
